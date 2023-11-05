8329debb3d5855f6aae35cdcdc7f0f00b0477f82
Yoann <41453809+YoannMourot@users.noreply.github.com>
2023-11-05 13:14:33 +0100

    test

--- interactui/app.js
+++ interactui/app.js
@@ -1,1 +1,21 @@
-console.log('toto');
+Promise.all([
+    navigator.permissions.query({ name: "accelerometer" }),
+    navigator.permissions.query({ name: "gyroscope" }),
+]).then((results) => {
+    if (results.every((result) => result.state === "granted")) {
+        const options = { frequency: 20, referenceFrame: "device" };
+        const sensor = new RelativeOrientationSensor(options);
+        sensor.addEventListener("reading", () => {
+            // model is a Three.js object instantiated elsewhere.
+            model.quaternion.fromArray(sensor.quaternion).inverse();
+          });
+          sensor.addEventListener("error", (error) => {
+            if (event.error.name === "NotReadableError") {
+              console.log("Sensor is not available.");
+            }
+          });
+          sensor.start();
+    } else {
+        console.log("No permissions to use RelativeOrientationSensor.");
+    }
+});
