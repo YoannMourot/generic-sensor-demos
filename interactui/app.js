0d6848b577e23260310a43b3eee25cb176351a2e
Yoann <41453809+YoannMourot@users.noreply.github.com>
2023-11-07 01:15:32 +0100

    Update app.js

--- interactui/app.js
+++ interactui/app.js
@@ -1,13 +1,14 @@
 Promise.all([
     navigator.permissions.query({ name: "accelerometer" }),
     navigator.permissions.query({ name: "gyroscope" }),
+    navigator.permissions.query({ name: "magnetometer" })
 ]).then((results) => {
     if (results.every((result) => result.state === "granted")) {
         const options = { frequency: 5, referenceFrame: "device" };
-        const sensor = new RelativeOrientationSensor(options);
+        const sensor = new AbsoluteOrientationSensor(options);
         sensor.addEventListener("reading", () => {
-            console.log(sensor.quaternion);
-            if(sensor.quaternion.x >= 0.3) {
+            console.log(sensor.quaternion); 
+            if(sensor.quaternion[0] >= 0.3) {
               document.getElementById( 'interactOutput' ).style.visibility = 'visible';
             } else {
               document.getElementById('interactOutput').style.visibility = 'hidden';
