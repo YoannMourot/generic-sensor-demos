469b18219ef201029a951bd08854e4e0cdb40927
Yoann <41453809+YoannMourot@users.noreply.github.com>
2023-11-07 01:12:31 +0100

    Update app.js

--- interactui/app.js
+++ interactui/app.js
@@ -3,11 +3,15 @@ Promise.all([
     navigator.permissions.query({ name: "gyroscope" }),
 ]).then((results) => {
     if (results.every((result) => result.state === "granted")) {
-        const options = { frequency: 20, referenceFrame: "device" };
+        const options = { frequency: 5, referenceFrame: "device" };
         const sensor = new RelativeOrientationSensor(options);
         sensor.addEventListener("reading", () => {
-            // model is a Three.js object instantiated elsewhere.
             console.log(sensor.quaternion);
+            if(sensor.quaternion.x >= 0.3) {
+              document.getElementById( 'interactOutput' ).style.visibility = 'visible';
+            } else {
+              document.getElementById('interactOutput').style.visibility = 'hidden';
+            }
           });
           sensor.addEventListener("error", (error) => {
             if (event.error.name === "NotReadableError") {
