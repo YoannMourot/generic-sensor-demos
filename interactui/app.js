c9ddd26b5940e1a70b0f2bdd0ffde503a28ccdab
Yoann <41453809+YoannMourot@users.noreply.github.com>
2023-11-08 23:59:14 +0100

    Update app.js

--- interactui/app.js
+++ interactui/app.js
@@ -6,23 +6,29 @@ if (navigator.permissions) {
       if (results.every((result) => result.state === "granted")) {
         const options = { frequency: 1, referenceFrame: "device" };
         const sensor = new RelativeOrientationSensor(options);
-            sensor.addEventListener("reading", () => {
-                console.log(sensor.quaternion); 
-                if(sensor.quaternion[0] >= 0.25) {
-                  document.getElementById( 'interactOutput' ).style.visibility = 'visible';
-                } else {
-                  document.getElementById('interactOutput').style.visibility = 'hidden';
-                }
-              });
-            sensor.addEventListener("error", (error) => {
-              if (event.error.name === "NotReadableError") {
-                console.log("Sensor is not available.");
-              }
-            });
-            sensor.start();
-        } else {
-            console.log("No permissions to use RelativeOrientationSensor.");
-        }
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+        const cube = new THREE.Mesh( geometry, material ); 
+      
+        sensor.addEventListener("reading", () => {
+          cube.quaternion.fromArray(sensor.quaternion).inverse();
+          cube.rotation.x = Math.PI / 2;
+          console.log(cube.quaternion);
+          if(cube.quaternion[1] >= 0.25) {
+            document.getElementById( 'interactOutput' ).style.visibility = 'visible';
+          } else {
+           document.getElementById('interactOutput').style.visibility = 'hidden';
+          }
+        });
+        sensor.addEventListener("error", (error) => {
+          if (event.error.name === "NotReadableError") {
+            console.log("Sensor is not available.");
+          }
+        });
+        sensor.start();
+      } else {
+        console.log("No permissions to use RelativeOrientationSensor.");
+      }
     }).catch(err => {
         console.log("Integration with Permissions API is not enabled, still try to start app.");
     });
